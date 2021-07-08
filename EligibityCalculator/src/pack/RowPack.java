7a5ae6634c92039a06cfcef2a7efcc618dfce53a
paultrau <paultrau@udel.edu>
2021-07-08 13:56:37 -0400

    first commit

---
+++ EligibityCalculator/src/pack/RowPack.java
@@ -0,0 +1,17 @@
+package pack;
+
+import java.io.File;
+import java.util.Date;
+
+public class RowPack {
+	boolean isFirst;
+	boolean isSecond;
+	boolean isThird;
+	
+	   
+	
+	public RowPack(File file, int index) {
+		
+	}
+
+}
